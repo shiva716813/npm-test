f3846f8308f1f731007e8942c4fbd6fd4a4e7a12
shiva716913 <shiva1ramezanian@gmail.com>
2023-05-16 12:50:51 +0430

    remove All

--- src/index.js
+++
@@ -1,4 +0,0 @@
-
-import Button from './lib/components/Button';
-import Badge from './lib/components/Badge';
-export {Badge , Button}
--- src/lib/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import Badge from './components/Badge';
-import Button from './components/Button';
-export { Badge, Button };
